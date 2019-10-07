f10381da950bfcc4cc909591250cd7a20cfa7566
b-d055 <32337174+b-d055@users.noreply.github.com>
2019-10-07 16:40:10 -0400

    Update StateChartLineOverlay.cs

--- StateChartLineOverlay.cs
+++ StateChartLineOverlay.cs
@@ -248,9 +248,17 @@ namespace ETS.Ts.Content
                             if (dataset.yAxisID === 'event') {
                                 return 'seconds';
                             } else {
-                                return ''
+                                return '';
                             }
                         },
+                        label: function(tooltipItem, data) {
+                            // only yLabel for speed, xLabel for events
+                            var dataset = data.datasets[tooltipItem.datasetIndex];
+                            if (dataset.yAxisID === 'line') {
+                                return tooltipItem.yLabel;
+                            } else {
+                                return tooltipItem.xLabel;
+                            }
                     }
                 },
                 elements: {
